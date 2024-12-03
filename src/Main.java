c7839aa87c1ee7b20d70f7c2bce5e427ac7bcf1b
Darya M <matchenko.darya@mail.ru>
2024-12-03 09:04:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int startingScore = 100;
+        int draft = 1100;
+
+        int bonus;
+        if (draft > 1000) {
+            bonus = draft /100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Итоговый бонус:" + bonus);
+    }
+}
